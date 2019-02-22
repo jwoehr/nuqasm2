d3811c87748fa271e0c233b2ef4d93c0702fcfff
Jack J. Woehr <jwoehr@softwoehr.com>
2019-02-22 06:33:27 -0700

    more on regression testing

--- runtest.sh
+++ runtest.sh
@@ -1,22 +1,44 @@
 # runtest.sh ... run some regression tests for nuqasm2
+
 LASTVER=$1
+
 if [ "$LASTVER" == "" ]
 then
 	echo "Usage: $0 LASTVER"
 	exit 1
 fi
-for i in test_qasm/g*.qasm test_qasm/if.qasm
+
+function diffout () {
+echo -n >outdiff.txt
+
+for i in *.out.txt
+do
+	echo $i >>outdiff.txt
+	echo "..." >>outdiff.txt
+	diff -u $i ${LASTVER}/$i >>outdiff.txt
+done
+}
+
+# run 'good' files
+for i in yiqing.qasm test_qasm/g*.qasm test_qasm/if.qasm
 do
 	FNAME=`basename $i .qasm`
 	python nuqasm2.py -o test_qasm/out/${FNAME}.out.txt -p --save_source --perf_filepath test_qasm/out/${FNAME}.perf.out.txt $i
 
 done
-cd test_qasm/out
-echo -n >outdiff.txt
-for i in *.out.txt
+
+# run erroneous files
+for i in test_qasm/err/*.qasm
 do
-	echo $i >>outdiff.txt
-	echo "..." >>outdiff.txt
-	diff -c $i ${LASTVER}/$i >>outdiff.txt
+	FNAME=`basename $i .qasm`
+	python nuqasm2.py -o test_qasm/err/out/${FNAME}.out.txt --save_source $i 2>test_qasm/err/out/${FNAME}.err.txt 
 done
+
+cd test_qasm/out
+diffout
+
+cd ../err/out
+diffout
+
+
 # end
